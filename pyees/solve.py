correct uncertanty calculation after setting element of variable

pyees/solve.py
@@ -150,12 +150,15 @@ def solve(func, x, *args, bounds = None,**kwargs):
         res = equation[0] - equation[1]
         res.convert(res._unitObject._SIBaseUnit)
         residuals.append(res)
-
+        
         ## loop over the variables
         for j, xj in enumerate(x):
             ## add the gradient d(residual)/d(xj) to the jacobian matrix
-            if xj in residuals[i].dependsOn.keys():
-                J[i,j] = residuals[i].dependsOn[xj]
+            for dependency in res.dependsOn:
+                if id(xj) == id(dependency[0]):
+                    J[i,j] += dependency[3]
+
+
 
     # inverse the jacobian
     Jinv = np.linalg.inv(J)

pyees/testVariable.py
@@ -237,7 +237,7 @@ class test(unittest.TestCase):
         C_vec = A_vec * B_vec
         np.testing.assert_array_equal(C_vec.value, np.array([12.3 * 745.1, 54.3 * 496.13, 91.3 * 120.54]))
         self.assertTrue(C._unitObject._assertEqual('L-m/min'))
-        np.testing.assert_array_equal(
+        np.testing.assert_array_almost_equal(
             C_vec.uncert,
             np.array([
                 np.sqrt((745.1 * 2.6)**2 + (12.3 * 53.9)**2),
@@ -282,7 +282,7 @@ class test(unittest.TestCase):
         C_vec = A_vec / B_vec
         np.testing.assert_array_equal(C_vec.value, np.array([12.3 / 745.1, 54.3 / 496.13, 91.3 / 120.54]))
         self.assertTrue(C_vec._unitObject._assertEqual('L/min-m'))
-        np.testing.assert_array_equal(
+        np.testing.assert_array_almost_equal(
             C_vec.uncert,
             np.array([
                 np.sqrt((1 / 745.1 * 2.6)**2 + (12.3 / (745.1)**2 * 53.9)**2),
@@ -1120,7 +1120,7 @@ class test(unittest.TestCase):
         unc = 2
         self.assertEqual(c.value, val * np.sin(np.pi / 180 * val))
         self.assertEqual(c.unit, 'deg')
-        self.assertEqual(c.uncert, np.sqrt((unc * (np.sin(np.pi / 180 * val) + (np.pi / 180 * val) * np.cos(np.pi / 180 * val)))**2))
+        self.assertAlmostEqual(c.uncert, np.sqrt((unc * (np.sin(np.pi / 180 * val) + (np.pi / 180 * val) * np.cos(np.pi / 180 * val)))**2))
 
         a = variable(23, 'deg', 2)
         a.convert('rad')
@@ -1150,7 +1150,7 @@ class test(unittest.TestCase):
         unc = 2
         self.assertEqual(c.value, val * np.sin(np.pi / 180 * val))
         self.assertEqual(c.unit, 'deg')
-        self.assertEqual(c.uncert, np.sqrt((unc * (np.sin(np.pi / 180 * val) + (np.pi / 180 * val) * np.cos(np.pi / 180 * val)))**2))
+        self.assertAlmostEqual(c.uncert, np.sqrt((unc * (np.sin(np.pi / 180 * val) + (np.pi / 180 * val) * np.cos(np.pi / 180 * val)))**2))
 
         a = variable(200, 'L/min', 1.5)
         b = a**2
@@ -1221,7 +1221,7 @@ class test(unittest.TestCase):
         a = variable(2.3, 'L/min', 0.0237)
         b = variable(0, 'L/min')
         for i in range(15):
-            b += 1 / variable(np.math.factorial(i)) * a
+            b += 1 / np.math.factorial(i) * a
         self.assertAlmostEqual(b.value, np.e * 2.3)
         self.assertEqual(b.unit, 'L/min')
         self.assertAlmostEqual(b.uncert, np.sqrt((np.e * 0.0237)**2))
@@ -1382,14 +1382,34 @@ class test(unittest.TestCase):
         b = a**2
         a[1] = variable(5,'m',0.5)
         c = b * a
-        
         a1 = variable([1,2,3], 'm', [0.1, 0.2, 0.3])
         a2 = variable([1,5,3], 'm', [0.1, 0.5, 0.3])
-        cc = a1*a1 * a2
+        cc = a1**2 * a2
         np.testing.assert_equal(c.value, cc.value)
         self.assertEqual(c.unit, cc.unit)          
         np.testing.assert_equal(c.uncert, cc.uncert)
-          
+        
+        
+        A = variable([1, 2, 3], 'L/min', [0.1, 0.2 ,0.3])
+        B = variable([93, 97, 102], 'Pa', [1.2, 2.4, 4.7])
+        A._addCovariance(B, [2, 3, 4])
+        C = A * B
+        A[1] = variable(2.5, 'L/min', 0.25)
+        C *= A
+        
+        AA = variable([1, 2, 3], 'L/min', [0.1, 0.2 ,0.3])
+        BB = variable([93, 97, 102], 'Pa', [1.2, 2.4, 4.7])
+        AA2 = variable([1, 2.5, 3], 'L/min', [0.1, 0.25, 0.3])
+        AA._addCovariance(BB, [2,3,4])
+        AA2._addCovariance(BB, [2,3,4])
+        CC = AA * BB * AA2
+        
+        np.testing.assert_equal(C.value, CC.value)
+        self.assertTrue(C._unitObject._assertEqual(CC.unit))
+        np.testing.assert_equal(C.uncert, CC.uncert)
+
+        
+        
           
            
     def testAppend(self):

pyees/variable.py
@@ -109,7 +109,7 @@ class scalarVariable():
         self._getConverterToSI()
 
         # uncertanty
-        self.dependsOn = {}
+        self.dependsOn = []
         self.covariance = {}
 
     def _getConverterToSI(self):
@@ -204,35 +204,28 @@ class scalarVariable():
                 return rf'{value} {pm} {uncert}{space}{unitStr}'
 
     def _addDependents(self, vars, grads):
-        # copy the gradients
-        # this prevents a pointer error as the gradients often are set to the value of a variable
-        # when the gradients are scaled this causes problems
         grads = [deepcopy(elem) for elem in grads]
-
+        selfScaleToSI = self._converterToSI.convert(1, useOffset=False)
+        
         # loop over the variables and their gradients
         for var, grad in zip(vars, grads):
+            
             # scale the gradient to SI units. This is necessary if one of the variables are converted after the dependency has been noted
-            scale = self._converterToSI.convert(1, useOffset=False) / var._converterToSI.convert(1, useOffset=False)
-            grad *= scale
+            grad *= selfScaleToSI / var._converterToSI.convert(1, useOffset=False)
 
-            # check if the variable depends on other variables
             if var.dependsOn:
-
+                # the variable depends on other variables
                 # loop over the dependencies of the variables and add them to the dependencies of self.
                 # this ensures that the product rule is used
-                for key, item in var.dependsOn.items():
-                    if key in self.dependsOn:
-                        self.dependsOn[key] += item * grad
-                    else:
-                        self.dependsOn[key] = item * grad
-            else:
-
+                for dependency in var.dependsOn:
+                    self.dependsOn.append([dependency[0], dependency[1], dependency[2], dependency[3] * grad])
+        
+            else:    
                 # the variable did not have any dependecies. Therefore the the varaible is added to the dependencies of self
-                if var in self.dependsOn:
-                    self.dependsOn[var] += grad
-                else:
-                    self.dependsOn[var] = grad
-
+                val = var._converterToSI.convert(var.value)
+                unc = var._converterToSI.convert(var.uncert, useOffset = False)
+                self.dependsOn.append([var,val, unc, grad])           
+                
     def _addCovariance(self, var, covariance):
         try:
             float(covariance)
@@ -245,27 +238,45 @@ class scalarVariable():
 
         # variance from each measurement
         variance = 0
+                
+        hasBeenUsed = [False] * len(self.dependsOn)
+        dependencies = []
+        for i, dependency_i in enumerate(self.dependsOn):
+            
+            if hasBeenUsed[i]: continue
+            hasBeenUsed[i] = True
+            grad = dependency_i[3]
+            
+            for j, dependency_j in enumerate(self.dependsOn):
+                if i == j: continue
+                if not id(dependency_i[0]) == id(dependency_j[0]): continue
+                if not np.array_equal(dependency_i[1], dependency_j[1]): continue
+                if not np.array_equal(dependency_i[2], dependency_j[2]): continue
+                hasBeenUsed[j] = True
+                grad += dependency_j[3]
+                
+            dependencies.append([dependency_i[0], dependency_i[1], dependency_i[2], grad])
+        
         selfScaleToSI = self._converterToSI.convert(1, useOffset=False)
-        for var, grad in self.dependsOn.items():
+        for dependency in dependencies:    
+            uncert, grad = dependency[2], dependency[3]
             # the gradient is scaled with the inverse of the conversion of the unit to SI units.
             # This is necessary if the variable "var" has been converted after the dependency has been noted
-            scale = var._converterToSI.convert(1, useOffset=False) / selfScaleToSI           
-            variance += (grad * scale * var.uncert)**2
+            scale = 1 / selfScaleToSI      
+            variance += (grad * scale * uncert)**2
 
         # variance from the corralation between measurements
-        listDependsOn = list(self.dependsOn.keys())
-        n = len(listDependsOn)
+        n = len(dependencies)
         for i in range(n):
-            var_i = list(listDependsOn)[i]
+            var_i = dependencies[i][0]
             for j in range(i + 1, n):
-                var_j = list(listDependsOn)[j]
+                var_j = dependencies[j][0]
                 if var_j in var_i.covariance.keys():
                     if not var_i in var_j.covariance.keys():
-                        raise ValueError(
-                            f'The variable {var_i} is correlated with the varaible {var_j}. However the variable {var_j} not not correlated with the variable {var_i}. Something is wrong.')
+                        raise ValueError(f'The variable {var_i} is correlated with the varaible {var_j}. However the variable {var_j} not not correlated with the variable {var_i}. Something is wrong.')
                     scale_i = var_i._converterToSI.convert(1, useOffset=False) / selfScaleToSI
                     scale_j = var_j._converterToSI.convert(1, useOffset=False) / selfScaleToSI
-                    varianceContribution = 2 * scale_i * self.dependsOn[var_i] * scale_j * self.dependsOn[var_j] * var_i.covariance[var_j]
+                    varianceContribution = 2 * scale_i * dependencies[i][3] * scale_j * dependencies[j][3] * var_i.covariance[var_j]
                     variance += varianceContribution
 
         self._uncert = np.sqrt(variance)
@@ -591,8 +602,10 @@ class scalarVariable():
     
     def max(self):
         return self
+    
     def min(self):
         return self
+    
     def mean(self):
         return self
 
@@ -923,9 +936,19 @@ def variable(value, unit = '', uncert = None, nDigits = 3):
         return scalarVariable(value, unit, uncert, nDigits)
 
 if __name__ == "__main__":
-    a = variable([1, 2, 3], '1', [0.1, 0.2 ,0.3])
-    b = variable([93, 97, 102], '1', [1.2, 2.4, 4.7])
-    a._addCovariance(b, [2, 3, 4])
-    c = a * b
+    A = variable([1, 2, 3], 'L/min', [0.1, 0.2 ,0.3])
+    B = variable([93, 97, 102], 'Pa', [1.2, 2.4, 4.7])
+    A._addCovariance(B, [2, 3, 4])
+    C = A * B
+    A[1] = variable(2.5, 'L/min', 0.25)
+    C *= A
     
-    print(c)
+    AA = variable([1, 2, 3], 'L/min', [0.1, 0.2 ,0.3])
+    BB = variable([93, 97, 102], 'Pa', [1.2, 2.4, 4.7])
+    AA2 = variable([1, 2.5, 3], 'L/min', [0.1, 0.25, 0.3])
+    AA._addCovariance(BB, [2,3,4])
+    AA2._addCovariance(BB, [2,3,4])
+    CC = AA * BB * AA2
+    
+    print(C.value, CC.value)
+    print(C.uncert, CC.uncert)