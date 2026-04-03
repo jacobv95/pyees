81c84fd3fb0f77b44340399e8fbd780930e8a1b9
jacobv95 <51778254+jacobv95@users.noreply.github.com>
2026-04-03 15:47:31 +0200

    removed files

--- compileDist.py
+++
@@ -1,3 +0,0 @@
-import subprocess
-subprocess.run("py -3.12 setup.py bdist --cythonize")
-subprocess.run("py -3.12 setup.py bdist")
--- uploadToPypi.py
+++
@@ -1,2 +0,0 @@
-import subprocess
-subprocess.run("twine upload --config-file ./.pypirc -r pypi --skip-existing dist/*")
